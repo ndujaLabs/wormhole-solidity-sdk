d8cfe4cd59d278c7d919b85e7177e26aa6464312
Francesco Sullo <sullof@gmail.com>
2024-03-07 15:10:04 -0800

    Remove useless hr

--- scripts/fix-readme.js
+++ scripts/fix-readme.js
@@ -13,7 +13,7 @@ const readmePath = path.join(__dirname, '../contracts/README.md');
 
 let readmeContent = fs.readFileSync(readmePath, 'utf8').split("\n");
 
-let insertText = `\n\n---\n\n### _This version of the Wormhole Solidity SDK, configured by [ndujaLabs](https://ndujalabs.com) for Hardhat compatibility, incorporates the original SDK as a submodule. It aligns with commit [${ellipsiseGitHash(hash)}](https://github.com/wormhole-foundation/wormhole-solidity-sdk/tree/${hash})._\n\n---\n\n`;
+let insertText = `\n\n### _This version of the Wormhole Solidity SDK, configured by [ndujaLabs](https://ndujalabs.com) for Hardhat compatibility, incorporates the original SDK as a submodule. It aligns with commit [${ellipsiseGitHash(hash)}](https://github.com/wormhole-foundation/wormhole-solidity-sdk/tree/${hash})._\n\n---\n\n`;
 
 readmeContent.splice(1, 0, insertText);
 let updatedReadmeContent = readmeContent.join("\n");
